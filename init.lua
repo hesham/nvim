1f595fc28db23a5bfec1daaab4a311fad729016b
Hesham El-Nahhas <16571+hesham@users.noreply.github.com>
2024-09-20 14:18:15 -0400

    Add clipboard

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
+-- Global Vim settings
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.wo.number = true
 vim.g.mapleader = " "
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 
 require("elna")
